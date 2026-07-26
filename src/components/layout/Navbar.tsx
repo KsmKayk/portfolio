510d9dd71f054abb0f0c483fec485f81a0321e7b
Kayk Mascarenhas <kaykdsg@gmail.com>
2026-07-26 11:39:32 -0300

    chore: remove lading-pages section from navbar (temp)

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -16,7 +16,7 @@ export function Navbar() {
         <ul className={`nav-links ${navOpen ? 'open' : ''}`}>
           {[
             ['sobre', '/#sobre'],['stack', '/#stack'], ['projetos', '/#projetos'],
-             ['landing pages', '/landing-pages'], ['contato', '/#contato'],
+            ['contato', '/#contato'], //['landing pages', '/landing-pages'], 
           ].map(([label, href]) => (
             <li key={href}>
               <a href={href} onClick={() => setNavOpen(false)}>{label}</a>
